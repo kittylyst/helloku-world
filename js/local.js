364c41222059c453d8c6cbe15539d69cfa16066e
Ben Evans <benjamin.john.evans@gmail.com>
2018-07-15 16:42:58 +0000

    Basic jQ setup

--- js/local.js
+++ js/local.js
@@ -4,4 +4,12 @@ function blah(date) {
     return {date: "Blep"}
 }
 
+function popup() {
+    alert( "Handler called." );
+  }
 
+  $(document).ready(function() {
+    $("#trigger").click(function() {
+        $("#demo").html("Hello, World!");
+    });
+});
--- myapp.py
+++ myapp.py
@@ -1,12 +1,16 @@
 import os
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
 
 # Returns JSON formatted date for AJAX-y goodness
 @app.route('/date')
@@ -15,4 +19,4 @@ def current_date():
 	
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
