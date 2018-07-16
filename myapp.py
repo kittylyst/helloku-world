a7025cb45fe428abc2332dba83cbef0f23eff928
Ben Evans <benjamin.john.evans@gmail.com>
2018-07-16 15:33:52 +0000

    Try to add Pg

--- myapp.py
+++ myapp.py
@@ -1,6 +1,8 @@
 import os
 import datetime
 import json
+import psycopg2
+import urllib.parse as urlparse
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
@@ -12,6 +14,29 @@ def index():
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('js', path)
+
+@app.route('/users')
+def get_users():
+    try:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        dbname = url.path[1:]
+        user = url.username
+        password = url.password
+        host = url.hostname
+        port = url.port
+        conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+        # conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
+        cur = conn.cursor()
+        cur.execute("""SELECT user_id, user_name from users""")
+        rows = cur.fetchall()
+        out = []
+        for row in rows:
+            out.append({"id": row[0], "name": row[1]})
+        return json.dumps(out)
+    except:
+        out = {"err": "Unable to connect to the database"}
+        return json.dumps(out)
+
 
 # Returns JSON formatted date for AJAX-y goodness
 @app.route('/date')
