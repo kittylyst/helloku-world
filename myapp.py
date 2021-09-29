291ef077073c8c3eb3f57626471d5d8c41e3b9e5
Ben Evans <ben@kathik.co.uk>
2021-09-29 21:22:41 +0200

    Cut back to basics

--- myapp.py
+++ myapp.py
@@ -2,7 +2,7 @@ import os
 import datetime
 import json
 import paho.mqtt.client as mqtt
-import psycopg2
+#import psycopg2
 import urllib.parse as urlparse
 from flask import Flask, render_template, send_from_directory, jsonify
 
@@ -16,30 +16,30 @@ def index():
 def send_js(path):
     return send_from_directory('js', path)
 
-@app.route('/users')
-def get_users():
-    conn = ""
-    out = []
-    try:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-        dbname = url.path[1:]
-        user = url.username
-        password = url.password
-        host = url.hostname
-        port = url.port
-        conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-    except:
-        out = {"err": "Unable to connect to the database"}
-    try:
-        cur = conn.cursor()
-        cur.execute("""SELECT user_id, user_name from users""")
-        rows = cur.fetchall()
-        out = []
-        for row in rows:
-            out.append({"id": row[0], "name": row[1]})
-    except:
-        out = {"err": "General SQL Error"}
-    return jsonify(out)
+# @app.route('/users')
+# def get_users():
+#     conn = ""
+#     out = []
+#     try:
+#         url = urlparse.urlparse(os.environ['DATABASE_URL'])
+#         dbname = url.path[1:]
+#         user = url.username
+#         password = url.password
+#         host = url.hostname
+#         port = url.port
+#         conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+#     except:
+#         out = {"err": "Unable to connect to the database"}
+#     try:
+#         cur = conn.cursor()
+#         cur.execute("""SELECT user_id, user_name from users""")
+#         rows = cur.fetchall()
+#         out = []
+#         for row in rows:
+#             out.append({"id": row[0], "name": row[1]})
+#     except:
+#         out = {"err": "General SQL Error"}
+#     return jsonify(out)
 
 
 # Returns JSON formatted date for AJAX-y goodness
